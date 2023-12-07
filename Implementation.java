211b37cfd2e6bbaaae2839a18684b2807da1072c
Ram Talla <ramcharantalla@gmail.com>
2023-12-07 11:57:54 -0500

    Changed the indents of main

--- Implementation.java
+++ Implementation.java
@@ -1,4 +1,5 @@
 public class Implementation implements Summation {
+
 	public int sum(int first, int last) {
 		int finalSum = 0;
 		for(int i = first; i <= last; i ++) {
@@ -6,4 +7,5 @@ public class Implementation implements Summation {
 		}
 		return finalSum;
 	}
+
 }
