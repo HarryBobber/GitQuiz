aaab13db3fd2507668408c80db04c0eac4329aea
Ram Talla <ramcharantalla@gmail.com>
2023-12-07 11:52:43 -0500

    This is the main

---
+++ Implementation.java
@@ -0,0 +1,9 @@
+public class Implementation implements Summation {
+	public int sum(int first, int last) {
+		int finalSum = 0;
+		for(int i = first; i <= last; i ++) {
+			finalSum += i;
+		}
+		return finalSum;
+	}
+}
